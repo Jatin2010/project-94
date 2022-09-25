b167f4d6ecdd5f2f97164ad06dc19959ad9fe06a
Jatin2010 <103494838+Jatin2010@users.noreply.github.com>
2022-09-25 16:58:39 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+ function addUser(){
+
+    login_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name" , login_name);
+    window.location = "kwitter_room.html" ;
+
+ }
---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyA4YIIub283AO6Zrsv5fG-CxAbIZ2DGK7I",
+        authDomain: "project-kwitter-3e8b1.firebaseapp.com",
+        databaseURL: "https://project-kwitter-3e8b1-default-rtdb.firebaseio.com",
+        projectId: "project-kwitter-3e8b1",
+        storageBucket: "project-kwitter-3e8b1.appspot.com",
+        messagingSenderId: "642144314600",
+        appId: "1:642144314600:web:f2f1ed4d7af9261940155d"
+      };
+      
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+
